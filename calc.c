ac1fea90fe6e6b3e66c5544638e498a779e28289
Brandon Vera <bbfei3535@gmail.com>
2024-10-08 19:31:32 -0500

    beginning

---
+++ calc.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+    printf("Hello wlecome to my simple calculator :)!");
+    printf("-----------------------------------------------");
+
+    printf("1. Addition");
+    printf("2. Subtraction");
+    printf("3. Multiplication");
+    printf("4. Division");
+
+    return 0;
+}
