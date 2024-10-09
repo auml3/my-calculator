116664e21b2cdf65493463359b36326d604efcc6
Brandon Vera <bbfei3535@gmail.com>
2024-10-08 19:49:30 -0500

    End +plus revision

--- calc.c
+++ calc.c
@@ -2,13 +2,14 @@
 
 int main(void)
 {
-    printf("Hello wlecome to my simple calculator :)!");
-    printf("-----------------------------------------------");
+    printf("     Hello welcome to my simple calculator :)!\n");
+    printf("✩-----------------------------------------------✩\n");
+    printf("\n");
 
-    printf("1. Addition");
-    printf("2. Subtraction");
-    printf("3. Multiplication");
-    printf("4. Division");
+    printf("1. Addition\n");
+    printf("2. Subtraction\n");
+    printf("3. Multiplication\n");
+    printf("4. Division\n");
     printf("\n");
 
     int menuop;
@@ -20,6 +21,22 @@ int main(void)
     scanf("%f", &value1);
     scanf("%f", &value2);
 
+    switch(menuop)
+    {
+        case 1:
+            printf("Your result is %f\n", value1+value2);
+            break;
+        case 2:
+            printf("Your result is %f\n", value1-value2);
+            break;
+        case 3:
+            printf("Your result is %f\n", value1*value2);
+            break;
+        case 4:
+            printf("Your result is %f\n", value1/value2);
+            break;            
+    }
+printf("✩-----------------------------------------------✩\n");
 
 
     return 0;
