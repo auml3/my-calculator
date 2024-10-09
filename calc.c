0e2e362dc850b9312e6a9a76a668caa3f8f7f9e2
Brandon Vera <bbfei3535@gmail.com>
2024-10-08 19:38:05 -0500

    middle

--- calc.c
+++ calc.c
@@ -9,6 +9,19 @@ int main(void)
     printf("2. Subtraction");
     printf("3. Multiplication");
     printf("4. Division");
+    printf("\n");
+
+    int menuop;
+    printf("What option would you like to choose?: ");
+    scanf("%d", &menuop);
+
+    float value1, value2;
+    printf("What two values would you like to use?: ");
+    scanf("%f", &value1);
+    scanf("%f", &value2);
+
+
 
     return 0;
+
 }
